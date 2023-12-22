a858c8304ade0eecc4475c36e7863ccfb9a4920e
Sujeet Kumar Gupta <sujeet.2024ec1161@kiet.edu>
2023-12-23 00:41:04 +0530

    update

--- src/service/api.js
+++ src/service/api.js
@@ -1,24 +1,20 @@
-
 import axios from "axios";
 
-const URL = "http://localhost:5000";
+const URL = "https://backend-orpin-kappa.vercel.app/";
 
 export const authenticateSignup = async (data) => {
-     try{
-        return await axios.post(`${URL}/api/signup`, data);
-    }catch(error){
-        console.log('Error while calling signup api', error);
-    }
-}
+  try {
+    return await axios.post(`${URL}/api/signup`, data);
+  } catch (error) {
+    console.log("Error while calling signup api", error);
+  }
+};
 
- export const authenticateLogin = async (data) => {
-     try{
-        return await axios.post(`${URL}/api/login`, data);
-    }catch(error){
-        console.log('Error while calling login api', error);
-        return error.response;
-    }
-}
-
-
-
+export const authenticateLogin = async (data) => {
+  try {
+    return await axios.post(`${URL}/api/login`, data);
+  } catch (error) {
+    console.log("Error while calling login api", error);
+    return error.response;
+  }
+};
